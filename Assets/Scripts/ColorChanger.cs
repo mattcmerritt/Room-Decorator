724df420c0680d883fc0ad32dd822735bd951a5f
mikepmerritt <mikepmerritt@gmail.com>
2022-03-24 01:31:25 -0400

    fixed issues with some of the color buttons not working

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class ColorChanger : MonoBehaviour
 {
     private string ColorName;
+    [SerializeField] private Material FlatColorMat;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,47 +21,59 @@ public class ColorChanger : MonoBehaviour
         
     }
 
-   public Color changeTheColor()
+   public void changeTheColor()
     {
         ColorName = EventSystem.current.currentSelectedGameObject.name;
+
+        Material newMat = new Material(FlatColorMat);
 
         switch (ColorName)
         {
             case "Red":
                 Debug.Log("its red");
-                return Color.red;
+                newMat.SetColor("_Color", Color.red);
+                gameObject.GetComponent<MeshRenderer>().material = newMat;
                 break;
             case "Yellow":
                 Debug.Log("its yellow");
-                return Color.yellow;
+                newMat.color = Color.yellow;
+                gameObject.GetComponent<MeshRenderer>().material = newMat;
                 break;
             case "Green":
                 Debug.Log("its green");
-                return Color.green;
+                newMat.color = Color.green;
+                gameObject.GetComponent<MeshRenderer>().material = newMat;
                 break;
             case "Blue":
                 Debug.Log("its blue");
-                return Color.blue;
+                newMat.color = Color.blue;
+                gameObject.GetComponent<MeshRenderer>().material = newMat;
                 break;
             case "Purple":
                 Debug.Log("its purple");
-                return new Color(128, 0, 128);
+                newMat.color = new Color(.5f, 0, .5f); // rgb 128, 0, 128
+                gameObject.GetComponent<MeshRenderer>().material = newMat;
                 break;
             case "Orange":
                 Debug.Log("its orange");
-                return new Color(255, 165, 0);
+                newMat.color = new Color(1, 0.392156862745098f, 0); // rgb 255, 100, 0
+                gameObject.GetComponent<MeshRenderer>().material = newMat;
                 break;
             case "Brown":
-                Debug.Log("its blue");
-                return new Color(165, 42, 42);
+                Debug.Log("its brown");
+                newMat.color = new Color(0.2823529411764706f, 0.1686274509803922f, 0.0431372549019608f); // rgb 72, 43, 11
+                gameObject.GetComponent<MeshRenderer>().material = newMat;
                 break;
             case "White":
                 Debug.Log("its white");
-                return Color.white;
+                newMat.color = Color.white;
+                gameObject.GetComponent<MeshRenderer>().material = newMat;
                 break;
 
             default:
-                return Color.white;
+                newMat.color = Color.white;
+                gameObject.GetComponent<MeshRenderer>().material = newMat;
+                break;
 
         }
        // Debug.Log("testing");
