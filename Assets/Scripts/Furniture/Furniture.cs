0118cf67c10eb2519bad5cbb889610776c401518
mattcmerritt <mattcmerritt@gmail.com>
2022-03-23 23:42:27 -0400

    basic movement of furniture with buttons

--- Assets/Scripts/Furniture/Furniture.cs
+++ Assets/Scripts/Furniture/Furniture.cs
@@ -8,9 +8,18 @@ public class Furniture : MonoBehaviour
     private Vector3 Size;
     private Color PaintColor;
 
+    [SerializeField, Range(0, 5)]
+    private float StepValue;
+
+    // Child objects
+    private GameObject MovementUI;
+
     private void Start()
     {
         Size = transform.localScale;
+        MovementUI = GetComponentInChildren<MovementUI>().gameObject;
+        // hiding inactive canvases
+        MovementUI.GetComponent<MovementUI>().HideUI();
     }
 
     public void ChangeColor(Color color)
@@ -18,8 +27,39 @@ public class Furniture : MonoBehaviour
         PaintColor = color;
     }
 
-    public void Move(Vector3 movement)
+    public void MoveForward()
     {
-        transform.position -= movement;
+        transform.position += Vector3.forward * StepValue;
+    }
+
+    public void MoveBackward()
+    {
+        transform.position += Vector3.back * StepValue;
+    }
+
+    public void MoveLeft()
+    {
+        transform.position += Vector3.left * StepValue;
+    }
+
+    public void MoveRight()
+    {
+        transform.position += Vector3.right * StepValue;
+    }
+
+    public void SelectFurniture()
+    {
+        MovementUI.SetActive(true);
+    }
+
+    public void DeselectFurniture()
+    {
+        MovementUI.SetActive(false);
+    }
+
+    public void OnMouseDown()
+    {
+        SelectFurniture();
+        GameManager.SelectFurniture(gameObject);
     }
 }
---
+++ Assets/Scripts/Furniture/MovementUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class MovementUI : MonoBehaviour
+{
+    private void Start()
+    {
+        // world space canvases need to be set up with the camera
+        Canvas canvas = GetComponent<Canvas>();
+        canvas.worldCamera = Camera.main;
+
+        // adding the onClick handlers to the button
+        Button[] buttons = GetComponentsInChildren<Button>();
+
+        Dictionary<string, UnityAction> behaviorSet = new Dictionary<string, UnityAction>();
+        behaviorSet.Add("left", () => GetComponentInParent<Furniture>().MoveLeft());
+        behaviorSet.Add("right", () => GetComponentInParent<Furniture>().MoveRight());
+        behaviorSet.Add("forward", () => GetComponentInParent<Furniture>().MoveForward());
+        behaviorSet.Add("backward", () => GetComponentInParent<Furniture>().MoveBackward());
+
+        foreach (Button b in buttons)
+        {
+            foreach (string key in behaviorSet.Keys)
+            {
+                if (b.name.ToLower().Contains(key))
+                {
+                    behaviorSet.TryGetValue(key, out UnityAction act);
+                    b.onClick.AddListener(act);
+                }
+            }
+        }
+    }
+
+    public void HideUI()
+    {
+        gameObject.SetActive(false);
+    }
+}
